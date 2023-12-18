86de34e9d29a6988d5a9535eb5a14c424d0546a2
Katareypooja <poojakatarey5@gmail.com>
2023-12-18 23:33:05 +0530

    files added

---
+++ springboot-backend/src/main/java/com/pooja/demo/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.pooja.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.pooja.demo.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> 
+{
+
+}
